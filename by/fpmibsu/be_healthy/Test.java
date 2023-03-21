a7b2b9808be44a7244a6fc44b74a42396e2232b5
Mishina Maria <pinina7773@gmail.com>
2023-03-21 19:36:59 +0300

    recipe category dao

--- by/fpmibsu/be_healthy/Test.java
+++ by/fpmibsu/be_healthy/Test.java
@@ -1,7 +1,9 @@
 package by.fpmibsu.be_healthy;
 
 import by.fpmibsu.be_healthy.dao.ArticleCategoryDao;
+import by.fpmibsu.be_healthy.dao.RecipeCategoryDao;
 import by.fpmibsu.be_healthy.entity.ArticleCategory;
+import by.fpmibsu.be_healthy.entity.RecipeCategory;
 
 import java.sql.SQLException;
 import java.util.*;
@@ -10,12 +12,22 @@ public class Test {
     public static void main(String[] args) {
         ArticleCategoryDao article_category_dao = new ArticleCategoryDao();
         ArticleCategory temp = new ArticleCategory();
-        temp.setId(2);
-        temp.setName("Семейный отдых");
+        temp.setId(3);
+        temp.setName("Беременность");
+        RecipeCategoryDao recipe_category_dao = new RecipeCategoryDao();
+        RecipeCategory temp1 = new RecipeCategory();
+        temp1.setId(1);
+        temp1.setName("Десерты");
         try {
-            article_category_dao.create(temp);
+            //article_category_dao.create(temp);
+            //recipe_category_dao.create(temp1);
             List<ArticleCategory> article_categories =  new ArticleCategoryDao().getAll();
+            List<RecipeCategory> recipe_categories =  new RecipeCategoryDao().getAll();
             for (ArticleCategory e : article_categories) {
+                System.out.println(e);
+            }
+            System.out.println("----------------------------------");
+            for (RecipeCategory e : recipe_categories) {
                 System.out.println(e);
             }
         } catch (SQLException e) {
---
+++ by/fpmibsu/be_healthy/dao/RecipeCategoryDao.java
@@ -0,0 +1,123 @@
+package by.fpmibsu.be_healthy.dao;
+import by.fpmibsu.be_healthy.bl.JDBCPostgreSQL;
+import by.fpmibsu.be_healthy.entity.RecipeCategory;
+
+import java.sql.*;
+import java.util.*;
+
+public class RecipeCategoryDao extends JDBCPostgreSQL implements Dao<RecipeCategory> {
+    private Connection connection = getConnection();
+    @Override
+    public List<RecipeCategory> getAll() throws SQLException {
+        List<RecipeCategory> projectList = new ArrayList<>();
+        String sql = "SELECT ID, NAME FROM RECIPE_CATEGORY";
+        Statement statement = null;
+        try {
+            statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                RecipeCategory category = new RecipeCategory();
+                category.setId(resultSet.getInt("ID"));
+                category.setName(resultSet.getString("NAME"));
+                projectList.add(category);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return projectList;
+    }
+    @Override
+    public RecipeCategory getEntityById(long id) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        String sql = "SELECT ID, NAME FROM RECIPE_CATEGORY WHERE ID=?";
+        RecipeCategory category = new RecipeCategory();
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            category.setId(resultSet.getInt("ID"));
+            category.setName(resultSet.getString("NAME"));
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return category;
+    }
+
+    @Override
+    public RecipeCategory update(RecipeCategory entity) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        String sql = "UPDATE RECIPE_CATEGORY SET NAME=? WHERE ID=?";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, entity.getName());
+            preparedStatement.setLong(2, entity.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return entity;
+    }
+
+    @Override
+    public void delete(RecipeCategory entity) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        String sql = "DELETE FROM RECIPE_CATEGORY WHERE ID=?";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, entity.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+    @Override
+    public void create(RecipeCategory entity) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        String sql = "INSERT INTO RECIPE_CATEGORY (ID, NAME) VALUES(?, ?)";
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, entity.getId());
+            preparedStatement.setString(2, entity.getName());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+}
