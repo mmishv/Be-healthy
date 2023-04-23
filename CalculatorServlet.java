f2f7d2bfe5a103a11f5fcf9f4e06258ece17cff8
mmishv <pinina7773@gmail.com>
2023-04-23 21:58:15 +0300

    add main jsp

--- CalculatorServlet.java
+++ CalculatorServlet.java
@@ -1,11 +1,15 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-public class TestServlet extends HttpServlet {
+public class CalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1> AAAAAAAAAAAAAAAAA </h1>");
+        pw.println("</html>");
     }
 
     @Override
