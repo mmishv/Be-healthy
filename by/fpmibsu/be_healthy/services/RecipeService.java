34b4a94780ffee3266420508d834149f89cb2b39
mmishv <pinina7773@gmail.com>
2023-04-27 23:44:04 +0300

    add recipe service

--- by/fpmibsu/be_healthy/services/RecipeService.java
+++ by/fpmibsu/be_healthy/services/RecipeService.java
@@ -1,10 +1,25 @@
 package by.fpmibsu.be_healthy.services;
-
-import by.fpmibsu.be_healthy.dao.Dao;
 import by.fpmibsu.be_healthy.dao.RecipeDao;
 import by.fpmibsu.be_healthy.entity.Recipe;
 import by.fpmibsu.be_healthy.pg.JDBCPostgreSQL;
 
-public class RecipeService extends RecipeDao {
+import java.sql.SQLException;
+import java.util.List;
 
+public class RecipeService {
+    public List<Recipe> getAll() throws SQLException{
+        return new RecipeDao().getAll();
+    };
+    public Recipe getEntityById(long id) throws SQLException{
+        return new RecipeDao().getEntityById(id);
+    };
+    public boolean update(Recipe entity) throws SQLException{
+        return new RecipeDao().update(entity);
+    }
+    public boolean delete(Recipe entity) throws SQLException{
+        return new RecipeDao().delete(entity);
+    }
+    public boolean create(Recipe entity) throws SQLException{
+        return new RecipeDao().create(entity);
+    }
 }
