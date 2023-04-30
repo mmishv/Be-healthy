20050cc8b03e611873e6edd1db6e9f626fe92801
mmishv <pinina7773@gmail.com>
2023-04-30 16:34:14 +0300

    add login, logout

--- module-info.java
+++
@@ -1,8 +0,0 @@
-module Be.healthy {
-    requires java.sql;
-    requires java.sql.rowset;
-    requires java.persistence;
-    requires org.hibernate.orm.core;
-    requires servlet.api;
-    requires com.fasterxml.jackson.databind;
-}
