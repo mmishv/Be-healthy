b35e4836d78ae8d4a463103e2b1277b065e1406b
mmishv <pinina7773@gmail.com>
2023-05-04 16:53:35 +0300

    add encoding filter

---
+++ by/fpmibsu/be_healthy/servlets/filters/EncodingFilter.java
@@ -0,0 +1,19 @@
+package by.fpmibsu.be_healthy.servlets.filters;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebFilter(filterName = "EncodingFilter")
+public class EncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        chain.doFilter(request, response);
+    }
+    @Override
+    public void destroy() {}
+}
