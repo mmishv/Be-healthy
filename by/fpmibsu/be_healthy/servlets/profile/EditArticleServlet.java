4ca04b4057e4f3a2671386862193146d7dae2808
mmishv <pinina7773@gmail.com>
2023-05-11 01:07:46 +0300

    add ability to edit articles

---
+++ by/fpmibsu/be_healthy/servlets/profile/EditArticleServlet.java
@@ -0,0 +1,71 @@
+package by.fpmibsu.be_healthy.servlets.profile;
+
+import by.fpmibsu.be_healthy.entity.Article;
+import by.fpmibsu.be_healthy.entity.ArticleCategory;
+import by.fpmibsu.be_healthy.services.ArticleCategoryService;
+import by.fpmibsu.be_healthy.services.ArticleService;
+import by.fpmibsu.be_healthy.services.RecipeService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.sql.Date.valueOf;
+
+@WebServlet(name = "EditArticleServlet", value = "/EditArticleServlet")
+public class EditArticleServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+        String[] pathParts = pathInfo.split("/");;
+        int article_id = Integer.parseInt(pathParts[pathParts.length-1]);
+        try {
+            Article article = new ArticleService().getEntityById(article_id);
+            request.setAttribute("categories", new ArticleCategoryService().getAll());
+            request.setAttribute("a_cats", article.getCategories().
+                    stream().map(ArticleCategory::getId).collect(Collectors.toList()));
+            request.setAttribute("article", article);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        getServletContext().getRequestDispatcher("/jsp/profile/edit_article.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<ArticleCategory> categories = new ArrayList<>();
+        var cats = request.getParameterValues("categories");
+        if (cats!=null){
+            for (var c: cats){
+                ArticleCategory cat = new ArticleCategory();
+                cat.setId(Integer.parseInt(c));
+                categories.add(cat);
+            }
+        }
+        Article article;
+        String pathInfo = request.getPathInfo();
+        String[] pathParts = pathInfo.split("/");;
+        int article_id = Integer.parseInt(pathParts[pathParts.length-1]);
+        try {
+            article = new ArticleService().getEntityById(article_id);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        article.setTitle(new String(request.getParameter("title").getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8));
+        article.setFulltext(new String(request.getParameter("text").getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8));
+        article.setCategories(categories);
+        try {
+            new ArticleService().update(article);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        response.sendRedirect("http://localhost:8081/my_articles/1");
+    }
+}
