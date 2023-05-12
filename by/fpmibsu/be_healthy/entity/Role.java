0add0bce39a00a9ae419236a8956f96d68494c2a
mmishv <pinina7773@gmail.com>
2023-05-12 16:50:25 +0300

    add role entity

---
+++ by/fpmibsu/be_healthy/entity/Role.java
@@ -0,0 +1,26 @@
+package by.fpmibsu.be_healthy.entity;
+public class Role {
+    private Integer id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role: "+ name;
+    }
+}
