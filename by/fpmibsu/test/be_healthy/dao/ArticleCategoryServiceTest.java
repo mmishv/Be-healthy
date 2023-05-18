3db29c66c7f1df8ff209178cfa3febbe3ed98480
mmishv <pinina7773@gmail.com>
2023-05-18 17:02:31 +0300

    complete article category service testing

--- by/fpmibsu/test/be_healthy/dao/ArticleCategoryServiceTest.java
+++ by/fpmibsu/test/be_healthy/dao/ArticleCategoryServiceTest.java
@@ -13,66 +13,133 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 
-public class ArticleCategoryServiceTest {
+public class ArticleCategoryServiceTest extends ArticleCategoryService {
     ArticleCategory updateCategory = new ArticleCategory(3, "Футбол"),
             beforeUpdateCategory = new ArticleCategory(3, "Беременность"),
-            newCategory = new ArticleCategory(6, "Йога");
+            newCategory = new ArticleCategory(6, "Йога"),
+            forDeleteCategory = new ArticleCategory(7, "Вред курения");
+
     @Before
     public void init() {
         DataSource.makeTest();
     }
+
     public List<ArticleCategory> createPositiveInput() {
-        return  Arrays.asList(
-                new ArticleCategory(3, "Беременность"),
+        return Arrays.asList(new ArticleCategory(3, "Беременность"),
                 new ArticleCategory(1, "Вегетерианство"),
                 new ArticleCategory(4, "Здоровое питание"),
                 new ArticleCategory(2, "Семейный отдых"),
-                new ArticleCategory(5, "Фитнес")
-        );
+                new ArticleCategory(5, "Фитнес"));
     }
+
     public List<ArticleCategory> createNegativeInput() {
-        return  Arrays.asList(
-                new ArticleCategory(1, "Веганство"),
+        return Arrays.asList(new ArticleCategory(1, "Веганство"),
                 new ArticleCategory(2, "Семейный отдых"),
                 new ArticleCategory(4, "Здоровое питание"),
-                new ArticleCategory(4, "Фитнес")
-        );
-    }
-    @Test
-    public void articleCategoryGetAllSizePositiveTest(){
-        assertEquals(createPositiveInput().size(),  new ArticleCategoryService().getAll().size());
-    }
-    @Test
-    public void articleCategoryGetAllSizeNegativeTest(){
-        assertNotEquals(createNegativeInput().size(),  new ArticleCategoryService().getAll().size());
-    }
-    @Test
-    public void articleCategoryGetAllPositiveTest(){
-        assertEquals("Категории должны быть лексикографически отсортированы", createPositiveInput(), new ArticleCategoryService().getAll());
-    }
-    @Test
-    public void articleCategoryGetAllNegativeTest(){
-        assertNotEquals(createNegativeInput(),  new ArticleCategoryService().getAll());
-    }
-    @Test
-    public void articleCategoryGetEntityByIdPositiveTest(){
-        assertEquals(new ArticleCategory(3, "Беременность"), new ArticleCategoryService().getEntityById(3));
+                new ArticleCategory(4, "Фитнес"));
     }
 
     @Test
-    public void articleCategoryGetEntityByNonExistentIdPositiveTest(){
-        assertNull(new ArticleCategoryService().getEntityById(10));
+    public void articleCategoryGetAllSizePositiveTest() {
+        assertEquals(createPositiveInput().size(), getAll().size());
+    }
+
+    @Test
+    public void articleCategoryGetAllSizeNegativeTest() {
+        assertNotEquals(createNegativeInput().size(), getAll().size());
+    }
+
+    @Test
+    public void articleCategoryGetAllPositiveTest() {
+        assertEquals("Категории должны быть лексикографически отсортированы",
+                createPositiveInput(), getAll());
+    }
+
+    @Test
+    public void articleCategoryGetAllNegativeTest() {
+        assertNotEquals(createNegativeInput(), getAll());
+    }
+
+    @Test
+    public void articleCategoryGetEntityByIdPositiveTest() {
+        assertEquals(new ArticleCategory(3, "Беременность"), getEntityById(3));
+    }
+
+    @Test
+    public void articleCategoryGetEntityByNonExistentIdPositiveTest() {
+        assertNull(getEntityById(10));
+    }
+
+    @Test
+    public void articleCategoryUpdatePositiveTest() {
+        assertTrue(new ArticleCategoryService().update(updateCategory));
+        assertEquals(updateCategory, getEntityById(updateCategory.getId()));
     }
     @Test
-    public void articleCategoryUpdateTest(){
-        boolean result = new ArticleCategoryService().update(updateCategory);
-        assertTrue(result);
-        assertEquals(updateCategory, new ArticleCategoryService().getEntityById(updateCategory.getId()));
-        boolean returnOld = new ArticleCategoryService().update(beforeUpdateCategory);
-        assertTrue(returnOld);
+    public void articleCategoryUpdateNonExistentNegativeTest() {
+         assertFalse(update(new ArticleCategory(100, "ЗОЖ")));
     }
+    @Test
+    public void articleCategoryUpdateNullNegativeTest() {
+        assertFalse(update(null));
+    }
+    @Test
+    public void articleCategoryCreatePositiveTest() {
+        assertTrue(new ArticleCategoryService().create(newCategory));
+        newCategory = getLastAdded();
+        assertEquals(newCategory, getEntityById(newCategory.getId()));
+    }
+
+    @Test
+    public void articleCategoryCreateCloneNegativeTest() {
+        assertFalse("Названия категорий должны быть уникальными",
+                create(new ArticleCategory(1, "Вегетерианство")));
+    }
+
+    @Test
+    public void articleCategoryCreateNullNegativeTest() {
+        assertFalse(create(null));
+    }
+    @Test
+    public void articleCategoryCreateNullNameNegativeTest() {
+        assertFalse("Названия категорий не могут быть null",
+                create(new ArticleCategory()));
+    }
+
+    @Test
+    public void articleCategoryDeletePositiveTest() {
+        create(forDeleteCategory);
+        forDeleteCategory = getLastAdded();
+        assertTrue(delete(forDeleteCategory.getId()));
+        assertNull(getEntityById(forDeleteCategory.getId()));
+    }
+
+    @Test
+    public void articleCategoryDeleteNonExistentNegativeTest() {
+        assertFalse(delete(100));
+    }
+    @Test
+    public void articleCategoryGetCategoriesByArticleIdSizePositiveTest() {
+        assertEquals(2, getArticleCategoriesByArticleId(2).size());
+    }
+
+    @Test
+    public void articleCategoryGetCategoriesByArticleIdPositiveTest() {
+        assertEquals(Arrays.asList(new ArticleCategory(2, "Семейный отдых"),
+                new ArticleCategory(3, "Беременность")),
+                getArticleCategoriesByArticleId(2));
+    }
+
+    private ArticleCategory getLastAdded() {
+        for (var t : getAll())
+            if (t.getId() > 5) return t;
+        return null;
+    }
+
     @After
-    public void returnData() {
-        new ArticleCategoryService().update(beforeUpdateCategory);
+    public void dataRecovery() {
+        update(beforeUpdateCategory);
+        delete(newCategory.getId());
+        DataSource.close();
     }
 }
