21cc6ffd25bf70ea470938ad07eb0bc1254e13ee
mmishv <pinina7773@gmail.com>
2023-05-18 21:39:16 +0300

    add constructor, hashCode and equals methods for recipe category

--- by/fpmibsu/be_healthy/entity/RecipeCategory.java
+++ by/fpmibsu/be_healthy/entity/RecipeCategory.java
@@ -1,10 +1,17 @@
 package by.fpmibsu.be_healthy.entity;
 import java.io.Serializable;
+import java.util.Objects;
 
 public class RecipeCategory implements Serializable {
     private int id;
     private String name;
-
+    public RecipeCategory(){
+        name = null;
+    }
+    public RecipeCategory(int id, String name){
+        this.name = name;
+        this.id = id;
+    }
     public int getId() {
         return id;
     }
@@ -19,6 +26,18 @@ public class RecipeCategory implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RecipeCategory that)) return false;
+        return getId() == that.getId() && Objects.equals(getName(), that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
     }
 
     @Override
